06e0146243cff19f7454fcea5aedc57032fa6260
Daniel-IesCaminas <113858967+Daniel-IesCaminas@users.noreply.github.com>
2022-11-17 20:12:48 +0100

    e6 con test

--- e6/test.php
+++ e6/test.php
@@ -4,7 +4,7 @@ require 'Gerente.php';
 require 'Programador.php';
 
 $empleado = new Empleado('20000000-A', 25000);
-
+$empleado-> __set(sueldo,20000);
 echo $empleado;
 
 $gerente = new Gerente('11111111-B');
